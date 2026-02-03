8f5f97d8744e1945bdd69f8c9e97c434ae6c2a39
Techzeke <dcfundz54@gmail.com>
2026-02-03 09:54:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let navbar = document.getElementById('nav-bar');
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 80) {
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+})
+
+function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.getElementById('navMenu');
+
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('show');
+
+    window.addEventListener('click', (e) => {
+    if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+        menu.classList.remove('show');
+        hamburger.classList.remove('active');
+    }
+    });
+}
